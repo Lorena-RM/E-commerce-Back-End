65e879efe9eea4f97c73a9199dcdfc45ba6fc219
Lorena Morales <Lorenarm.999@gmail.com>
2022-11-03 20:32:30 -0600

    delete route or product working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -104,6 +104,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({where: {id: req.params.id}})
+  .then(()=>{
+    res.status(200).json({message:'product deleted!'})
+  })
+  .catch((err)=>
+  res.status(400).json(err)
+  )
 });
 
 module.exports = router;
